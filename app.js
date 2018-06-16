173fbf879e655cf2738f696bf924b7d06ca9a4ec
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-06-16 03:30:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,142 @@
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+const dynamicStatic = require('express-dynamic-static')(); // immediate initialization
+
+mongoose.connection.on('error', function (err) { console.log(err) });
+mongoose.connect(process.env.DATABASE_URL);
+
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+let expressSanitized = require('express-sanitize-escape');
+let moment = require("moment");
+var fs = require('fs');
+
+console.log("Launched on " + moment().format("LLLL"));
+
+let app = express();
+
+app.use(session({
+    secret: 'asdafjgldigldifgj3adk6alsdgkpfgkd',
+    saveUninitialized: true,
+    resave: false
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+    next();
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressSanitized.middleware());
+app.use(cookieParser());
+
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(dynamicStatic);
+
+dynamicStatic.setPath(path.join(__dirname, 'public'));
+
+let index = require('./routes/index');
+app.use('/', index);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+let debug = require('debug')('boilerplate:server');
+
+/**
+ * Create HTTP server.
+ */
+var http = require('http');
+let server = http.createServer(app).listen(parseInt(process.env.PORT));
+let port = normalizePort(process.env.PORT);
+app.set('port', port);
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    let port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+    res.render('index');
+});
+
+module.exports = router;
